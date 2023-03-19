6784d5caa42eebdad1ee1a2eb2be432e069c6665
riyandifirman <riyandi.firman08@gmail.com>
2023-03-19 14:00:43 +0700

    finishing simple view model

---
+++ app/src/main/java/com/riyandifirman/myviewmodel/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.riyandifirman.myviewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, length: String, height: String) {
+        result = width.toInt() * length.toInt() * height.toInt()
+    }
+}
